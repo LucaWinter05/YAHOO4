fc8a809328f69786f0abbe657e9fe989a2fbb8c1
Lucawinterottode <Luca.winter@otto.de>
2026-09-14 21:48:57 +0200

    hhh

---
+++ mein_programm.py
@@ -0,0 +1,12 @@
+# print("Hello, luca!")
+# alter = int(input("Wie alt bist du? "))
+# print("Du bist", alter, "Jahre alt.")
+# if alter < 0:
+#     print("Ungültiges Alter.")
+# if alter < 18 and alter >= 0:
+#     print("Du bist minderjährig.")
+# elif alter >= 18:
+#     print("Du bist volljährig.")
+
+
+
