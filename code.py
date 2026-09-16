9100bde5f9da74e76bf108dff8bca22341038df1
maximhelmerotto <maxim.helmer@otto.de>
2026-09-16 13:19:57 +0200

    w

--- code.py
+++ code.py
@@ -3,16 +3,15 @@ import yfinance as yf
 
 
 class get_data:
-    def __init__(self,übergabe):
+    def __init__(self, übergabe):
 
-        self.daten=übergabe
+        self.daten = übergabe
 
         self.suche = yf.Search(self.daten)
         self.ticker = self.suche.quotes[0]['symbol']
 
-
         self.aktie = yf.Ticker(self.ticker)
-        self.preis = self.aktie.fast_fast_info['lastPrice']
-        self.marktkapitalisierung = self.aktie.fast_info['marketCap']
+        self.preis = self.aktie.fast_info['last_price']
+        self.marktkapitalisierung = self.aktie.fast_info['market_cap']
         # print(f"Eine Aktie kostet von {self.suche.quotes[0]['longname']} / {self.ticker} kostet {round(self.aktie.fast_info['lastPrice'], 2)}$")
         # print(f"Die Marktkapitalisierung von {self.suche.quotes[0]['longname']} / {self.ticker} beträgt {round(self.aktie.fast_info['marketCap'], 2)}$")
