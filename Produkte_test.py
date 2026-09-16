f095401732453f8cc0ed488861e745a6607bffbe
jonasschneider-otto <jonas.schneider@otto.de>
2026-09-16 13:14:47 +0200

    qf

--- Produkte_test.py
+++
@@ -1,6 +0,0 @@
-import produkte
-
-meins = produkte.rand_prod()
-print(meins.get_preis())
-print(meins.calc_wert(150))
-print("Für diese 150 Euro könntest du dir auch " + str(meins.calc_wert(150))  + " mal \"" + str(meins.get_produkt()) + "\" (" + str(meins.get_preis()) + "€) auf otto.de kaufen")
