7a898de57476861f69da4367d46ebe771084c6de
Lucawinterottode <Luca.winter@otto.de>
2026-09-16 12:53:36 +0200

    hdh

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ class get_data:
 
 
         self.aktie = yf.Ticker(self.ticker)
-        self.preis = self.aktie.fast_fast_info['lastPrice']
+        self.preis = self.aktie.fast_info['lastPrice']
         self.marktkapitalisierung = self.aktie.fast_info['marketCap']
         # print(f"Eine Aktie kostet von {self.suche.quotes[0]['longname']} / {self.ticker} kostet {round(self.aktie.fast_info['lastPrice'], 2)}$")
         # print(f"Die Marktkapitalisierung von {self.suche.quotes[0]['longname']} / {self.ticker} beträgt {round(self.aktie.fast_info['marketCap'], 2)}$")
