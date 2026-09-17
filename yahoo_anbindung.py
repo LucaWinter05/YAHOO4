9a281da33b87c356644103274cde6e70c3c865d0
maximhelmerotto <maxim.helmer@otto.de>
2026-09-17 13:49:41 +0200

    aktuell währung

--- yahoo_anbindung.py
+++ yahoo_anbindung.py
@@ -1,5 +1,7 @@
 
+
 import yfinance as yf
+import currency_converter as cc
 
 
 
@@ -7,22 +9,28 @@ class get_data:
     def __init__(self, übergabe):
 
         self.daten = übergabe
-
+        converter = cc.CurrencyConverter()
         self.suche = yf.Search(self.daten)
         quote = self.suche.quotes[0]
         self.ticker = quote['symbol']
         self.ist_aktie = quote.get('quoteType') in ('EQUITY', 'STOCK')
         self.aktie = yf.Ticker(self.ticker)
-        self.preis = self.aktie.fast_info['last_price']
         self.währung = self.aktie.fast_info['currency']
+        self.preis = converter.convert( self.aktie.fast_info['last_price'], self.währung, 'EUR'
+        )
+        self.währung = "EUR"
+        
         self.historie = self.aktie.history(period="1y")[['Close']]
+
         if self.ist_aktie:
-            self.marktkapitalisierung = self.aktie.fast_info['market_cap']
+            self.marktkapitalisierung = converter.convert(self.aktie.fast_info['market_cap'], self.währung, 'EUR')
+            
         else:
-            self.fondsgröße = (self.aktie.info.get('totalAssets') or
-                               self.aktie.info.get('netAssets') or
-                               quote.get('totalAssets') or
-                               quote.get('netAssets'))
+            fondsgröße = (self.aktie.info.get('totalAssets') or
+                          self.aktie.info.get('netAssets') or
+                          quote.get('totalAssets') or
+                          quote.get('netAssets'))
+            
         # print(f"Eine Aktie kostet von {self.suche.quotes[0]['longname']} / {self.ticker} kostet {round(self.aktie.fast_info['lastPrice'], 2)}$")
         # print(f"Die Marktkapitalisierung von {self.suche.quotes[0]['longname']} / {self.ticker} beträgt {round(self.aktie.fast_info['marketCap'], 2)}$")
 
